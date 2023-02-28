1b7d69f2a14a8e34397adda1d907999ed690cc1f
Gustavo Della Giustina <gustavodg553@gmail.com>
2023-02-28 09:00:12 -0300

    fixacao-1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop_for/scripts.js
@@ -0,0 +1,17 @@
+/* let pizzas = ['4queijos', 'frangoCatupiry', 'margherita', 'palmito', 'chocolate'];
+
+pizzas.push('peitodePEru');
+pizzas.push('gratinada');
+
+for (let index = 0; index < pizzas.length; index += 1) {
+    console.log(pizzas[index]);
+} */
+
+let pizzas = ['a', 'f', 'p'];
+//pizzas.push('ar');
+//pizzas.unshift('bol');
+//pizzas.pop();
+//pizzas.shift();
+console.log(pizzas.length);
+console.log(pizzas);
+console.log(pizzas.indexOf('p'));
