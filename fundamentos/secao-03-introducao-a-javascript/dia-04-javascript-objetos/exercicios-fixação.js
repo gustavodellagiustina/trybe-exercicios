554e76e4705c76d5fffe2ebd3a6d81404b90c090
Gustavo Della Giustina <gustavodg553@gmail.com>
2023-03-05 21:33:00 -0300

    pedido de clientes

--- fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios-fixação.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos/exercicios-fixação.js
@@ -204,7 +204,7 @@ ${verificaRecorrentes()}`);
 
 // ============================================
 
-
+/*
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
@@ -217,6 +217,7 @@ let leitor = {
         },
     ],
 };
+*/
 
 // ============================================
 
@@ -228,11 +229,13 @@ let leitor = {
 
 // Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
 
+/*
 leitor.livrosFavoritos.push({
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
     autor: 'JK Rowling',
     editora: 'Rocco',
 });
+*/
 
 // console.log(leitor);
 
@@ -241,6 +244,79 @@ leitor.livrosFavoritos.push({
 // Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, em que “<quantidade>” corresponde à quantidade de livros favoritos e é um número gerado automaticamente por seu código.
 
 // console.log(`"${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos”, em que “${leitor.livrosFavoritos.length}” corresponde à quantidade de livros favoritos e é um número gerado automaticamente por seu código.`);
+
+// ============================================
+
+// Pedido de clientes
+// Imagine que você seja responsável por cuidar do sistema de entrega de um restaurante e que precise enviar mensagens com as informações da compra. Para isso, use o seguinte código:
+
+/*
+let order = {
+    name: 'Rafael Andrade',
+    phoneNumber: '11-98763-1416',
+    address: {
+        street: 'Rua das Flores',
+        number: '389',
+        apartment: '701',
+    },
+    order: {
+        pizza: {
+            marguerita: {
+                amount: 1,
+                price: 25,
+            },
+            pepperoni: {
+                amount: 1,
+                price: 20,
+            },
+        },
+        drinks: {
+            coke: {
+                type: 'Coca-Cola Zero',
+                price: 10,
+                amount: 1,
+            },
+        },
+        delivery: {
+            deliveryPerson: 'Ana Silveira',
+            price: 5,
+        },
+    },
+    payment: {
+        total: 60,
+    },
+};
+*/
+
+// ============================================
+
+// Complete a função customerInfo() para que seu retorno seja similar a 'Olá, Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701'.
+
+/*
+function customerInfo(order) {
+    let message;
+    return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
+}
+
+console.log(customerInfo(order));
+*/
+
+// ============================================
+
+// Complete a função orderModifier() para que seu retorno seja similar a 'Olá, Luiz Silva, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.';
+// Modifique o nome da pessoa compradora para Luiz Silva;
+// Modifique o valor total da compra para R$ 50,00.
+
+/*
+function orderModifier(order) {
+    order.name = 'Luiz Silva';
+    order.payment.total = 50;
+    let pizzaFlavors = Object.keys(order.order.pizza);
+    return `Olá, ${order.name}, o valor total de seu pedido de ${pizzaFlavors[0]}, ${pizzaFlavors[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`
+}
+
+console.log(orderModifier(order));
+*/
 
 // ============================================
 
